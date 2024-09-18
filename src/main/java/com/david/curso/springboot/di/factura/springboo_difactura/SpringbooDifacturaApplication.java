a7411b9dc7a095f5a50b2b4bec9c0c5d2c866591
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 17:49:41 -0600

    creacion del proyecto

---
+++ src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/SpringbooDifacturaApplication.java
@@ -0,0 +1,13 @@
+package com.david.curso.springboot.di.factura.springboo_difactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooDifacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbooDifacturaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/david/curso/springboot/di/factura/springboo_difactura/SpringbooDifacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.david.curso.springboot.di.factura.springboo_difactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooDifacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
