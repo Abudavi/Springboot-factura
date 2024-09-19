1779ec957419cc1f7be81097d320cd8a81f2daa8
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 22:53:23 -0600

    se creo un bean con productos e items

--- src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/AppConfig.java
+++ src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/AppConfig.java
@@ -1,10 +1,27 @@
 package com.david.curso.springboot.di.factura.springboo_difactura;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.david.curso.springboot.di.factura.springboo_difactura.models.Item;
+import com.david.curso.springboot.di.factura.springboo_difactura.models.Product;
 
 @Configuration
 @PropertySource(value = "classpath:data.properties", encoding = "UTF-8")
 public class AppConfig {
 
+    @Bean
+    List<Item> itemsInvoice() {
+        Product p1 = new Product("Camara sony", 800);
+        Product p2 = new Product("Bicicleta 26", 1200);
+        List<Item> items = Arrays.asList(
+                new Item(p1, 2),
+                new Item(p2, 3));
+
+        return items;
+    }
 }
