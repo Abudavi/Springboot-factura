3b0fd8d775a4ad73404d8ab6656aafb7c04ecf64
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 23:25:53 -0600

    se agrego un metodo con items diferentes y se especifico con qualifier

--- src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/AppConfig.java
+++ src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/AppConfig.java
@@ -3,8 +3,10 @@ package com.david.curso.springboot.di.factura.springboo_difactura;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 
 import com.david.curso.springboot.di.factura.springboo_difactura.models.Item;
@@ -15,12 +17,29 @@ import com.david.curso.springboot.di.factura.springboo_difactura.models.Product;
 public class AppConfig {
 
     @Bean
+    @Qualifier
     List<Item> itemsInvoice() {
         Product p1 = new Product("Camara sony", 800);
         Product p2 = new Product("Bicicleta 26", 1200);
         List<Item> items = Arrays.asList(
                 new Item(p1, 2),
                 new Item(p2, 3));
+
+        return items;
+    }
+
+    @Bean
+    @Qualifier("itemsOficce")
+    List<Item> itemsInvoiceOffice() {
+        Product p1 = new Product("Monitor asus 24", 700);
+        Product p2 = new Product("Notebook razer", 2400);
+        Product p3 = new Product("Impresora hp", 800);
+        Product p4 = new Product("Escritorio oficina", 800);
+        List<Item> items = Arrays.asList(
+                new Item(p1, 2),
+                new Item(p2, 3),
+                new Item(p3, 6),
+                new Item(p4, 4));
 
         return items;
     }
