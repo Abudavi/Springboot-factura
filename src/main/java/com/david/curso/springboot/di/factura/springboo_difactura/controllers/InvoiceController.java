901a2e7e4be54c7c4e375b656b45c70f780c3bcf
David MTZD <davidmtzd21512@gmail.com>
2024-09-18 23:26:24 -0600

    se creo la clase para enviar datos por apirest

---
+++ src/main/java/com/david/curso/springboot/di/factura/springboo_difactura/controllers/InvoiceController.java
@@ -0,0 +1,23 @@
+package com.david.curso.springboot.di.factura.springboo_difactura.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.david.curso.springboot.di.factura.springboo_difactura.models.Invoice;
+
+@RestController
+@RequestMapping("/invoice")
+public class InvoiceController {
+
+    @Autowired
+    private Invoice invoice;
+
+    @GetMapping("/show")
+    public Invoice show() {
+
+        return invoice;
+    }
+
+}
